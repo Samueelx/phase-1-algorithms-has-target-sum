256f2dc75e0822c78c65a7ad7e85ded4f104bd18
Samueelx <gsammygitau@gmail.com>
2023-01-05 11:22:34 +0300

    completed assignment.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let sum = 0;
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      sum = array[i] + array[j];
+      if(sum === target)
+      return true;
+      else sum = 0;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  Given an input array of n elements, the worst case scenario is that 
+  the algorithm needs to make n^2 iterations.
 */
 
 /* 
   Add your pseudocode here
+
+  take one number from the input array
+    add it to each of the other numbers in the input array to find the sum
+      compare the sum to the target
+        if they are equal -> return true
+        if none of the sums is equals to the target -> return false  
 */
 
 /*
